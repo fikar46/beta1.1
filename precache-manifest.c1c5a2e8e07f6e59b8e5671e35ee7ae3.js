0e08251df0fb5bf491fd44fa8581e3430728b0c2
fikar46 <43537558+fikar46@users.noreply.github.com>
2019-05-03 16:09:41 +0700

    build beta 1.1

---
+++ precache-manifest.c1c5a2e8e07f6e59b8e5671e35ee7ae3.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b464e274bc331f9a765d765359635a5",
+    "url": "/static/media/fa-brands-400.7b464e27.woff"
+  },
+  {
+    "revision": "201f3d7ba8dc2dc57e80",
+    "url": "/static/css/main.75e988df.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "534ded7bf4c51f5ef700",
+    "url": "/static/js/2.ac5042c5.chunk.js"
+  },
+  {
+    "revision": "949a2b066ec37f5a384712fc7beaf2f1",
+    "url": "/static/media/fa-regular-400.949a2b06.woff2"
+  },
+  {
+    "revision": "381af09a1366b6c2ae65eac5dd6f0588",
+    "url": "/static/media/fa-regular-400.381af09a.woff"
+  },
+  {
+    "revision": "7422060ca379ee9939d3b687d072acad",
+    "url": "/static/media/fa-regular-400.7422060c.eot"
+  },
+  {
+    "revision": "73fe7f1effbf382f499831a0a9f18626",
+    "url": "/static/media/fa-regular-400.73fe7f1e.ttf"
+  },
+  {
+    "revision": "48461ea4e797c9774dabb4a0440d2f56",
+    "url": "/static/media/fa-brands-400.48461ea4.woff2"
+  },
+  {
+    "revision": "14a08198ec7d1eb96d515362293fed36",
+    "url": "/static/media/fa-solid-900.14a08198.woff2"
+  },
+  {
+    "revision": "201f3d7ba8dc2dc57e80",
+    "url": "/static/js/main.41abaf92.chunk.js"
+  },
+  {
+    "revision": "815694de1120d6c1e9d1f0895ee81056",
+    "url": "/static/media/fa-solid-900.815694de.woff"
+  },
+  {
+    "revision": "9b6c8da3c489424e2b3e9c9fb6314b37",
+    "url": "/static/media/fa-brands-400.9b6c8da3.eot"
+  },
+  {
+    "revision": "947b9537bc0fecc8130d48eb753495a1",
+    "url": "/static/media/fa-brands-400.947b9537.ttf"
+  },
+  {
+    "revision": "6d4774d4e483e03057f9d09544656b42",
+    "url": "/static/media/fa-regular-400.6d4774d4.svg"
+  },
+  {
+    "revision": "70e65a7d34902f2c350816ecfe2f6492",
+    "url": "/static/media/fa-solid-900.70e65a7d.eot"
+  },
+  {
+    "revision": "0079a0ab6bec4da7d6e16f2a2e87cd71",
+    "url": "/static/media/fa-solid-900.0079a0ab.ttf"
+  },
+  {
+    "revision": "3897e68864e7fe1fbb468c0220573010",
+    "url": "/static/media/fa-brands-400.3897e688.svg"
+  },
+  {
+    "revision": "3fad11457f70f880252e0ab5c9cee82e",
+    "url": "/static/media/fa-solid-900.3fad1145.svg"
+  },
+  {
+    "revision": "030e231563d647eecc0bb570b8f47331",
+    "url": "/index.html"
+  }
+];
